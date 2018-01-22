a0654ecae394ceb360642a4e9024aecede34f7ed
lcd1232 <8745863+lcd1232@users.noreply.github.com>
2018-01-23 02:17:11 +0300

    Reformat code

--- zxing/__init__.py
+++ zxing/__init__.py
@@ -12,106 +12,108 @@
 __version__ = '0.3'
 import subprocess, re, os
 
-class BarCodeReader():
-  location = ""
-  command = "java"
-  libs = ["javase/javase.jar", "core/core.jar"]
-  args = ["-cp", "LIBS", "com.google.zxing.client.j2se.CommandLineRunner"]
 
-  def __init__(self, loc=""):
-    if not len(loc):
-      if (os.environ.has_key("ZXING_LIBRARY")):
-        loc = os.environ["ZXING_LIBRARY"]
-      else:
-        loc = ".."
+class BarCodeReader(object):
+    location = ""
+    command = "java"
+    libs = ["javase.jar", "core.jar", "jcommander.jar"]
+    args = ["-cp", "LIBS", "com.google.zxing.client.j2se.CommandLineRunner"]
 
-    self.location = loc
+    def __init__(self, path=None):
+        if path is None:
+            if os.environ.get("ZXING_LIBRARY"):
+                path = os.environ["ZXING_LIBRARY"]
+            else:
+                path = ".."
 
-  def decode(self, files, try_harder = False, qr_only = False):
-    cmd = [self.command]
-    cmd += self.args[:] #copy arg values
-    if try_harder:
-      cmd.append("--try_harder")
-    if qr_only:
-      cmd.append("--possibleFormats=QR_CODE")
+        self.location = path
 
-    libraries = [self.location + "/" + l for l in self.libs]
+    def decode(self, files, try_harder=False, qr_only=False):
+        cmd = [self.command]
+        cmd += self.args[:]  # copy arg values
+        if try_harder:
+            cmd.append("--try_harder")
+        if qr_only:
+            cmd.append("--possibleFormats=QR_CODE")
 
-    cmd = [ c if c != "LIBS" else os.pathsep.join(libraries) for c in cmd ]
+        libraries = [self.location + "/" + l for l in self.libs]
 
-    # send one file, or multiple files in a list
-    SINGLE_FILE = False
-    if type(files) != type(list()):
-      cmd.append(files)
-      SINGLE_FILE = True
-    else:
-      cmd += files
+        cmd = [c if c != "LIBS" else os.pathsep.join(libraries) for c in cmd]
 
-    (stdout, stderr) = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True).communicate()
-    codes = []
-    file_results = stdout.split("\nfile:")
-    for result in file_results:
-      lines = stdout.split("\n")
-      if re.search("No barcode found", lines[0]):
-        codes.append(None)
-        continue
+        # send one file, or multiple files in a list
+        SINGLE_FILE = False
+        if type(files) != type(list()):
+            cmd.append(files)
+            SINGLE_FILE = True
+        else:
+            cmd += files
 
-      codes.append(BarCode(result))
+        (stdout, stderr) = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True).communicate()
+        codes = []
+        file_results = stdout.split("\nfile:")
+        for result in file_results:
+            lines = stdout.split("\n")
+            if re.search("No barcode found", lines[0]):
+                codes.append(None)
+                continue
 
-    if SINGLE_FILE:
-      return codes[0]
-    else:
-      return codes
+            codes.append(BarCode(result))
 
-#this is the barcode class which has
+        if SINGLE_FILE:
+            return codes[0]
+        else:
+            return codes
+
+
+# this is the barcode class which has
 class BarCode:
-  format = ""
-  points = []
-  data = ""
-  raw = ""
+    format = ""
+    points = []
+    data = ""
+    raw = ""
 
-  def __init__(self, zxing_output):
-    lines = zxing_output.split("\n")
-    raw_block = False
-    parsed_block = False
-    point_block = False
-
-    self.points = []
-    for l in lines:
-      m = re.search("format:\s([^,]+)", l)
-      if not raw_block and not parsed_block and not point_block and m:
-        self.format = m.group(1)
-        continue
-
-      if not raw_block and not parsed_block and not point_block and l == "Raw result:":
-        raw_block = True
-        continue
-
-      if raw_block and l != "Parsed result:":
-        self.raw += l + "\n"
-        continue
-
-      if raw_block and l == "Parsed result:":
+    def __init__(self, zxing_output):
+        lines = zxing_output.split("\n")
         raw_block = False
-        parsed_block = True
-        continue
-
-      if parsed_block and not re.match("Found\s\d\sresult\spoints", l):
-        self.data += l + "\n"
-        continue
-
-      if parsed_block and re.match("Found\s\d\sresult\spoints", l):
         parsed_block = False
-        point_block = True
-        continue
+        point_block = False
 
-      if point_block:
-        m = re.search("Point\s(\d+):\s\(([\d\.]+),([\d\.]+)\)", l)
-        if (m):
-          self.points.append((float(m.group(2)), float(m.group(3))))
+        self.points = []
+        for l in lines:
+            m = re.search("format:\s([^,]+)", l)
+            if not raw_block and not parsed_block and not point_block and m:
+                self.format = m.group(1)
+                continue
 
-    return
+            if not raw_block and not parsed_block and not point_block and l == "Raw result:":
+                raw_block = True
+                continue
+
+            if raw_block and l != "Parsed result:":
+                self.raw += l + "\n"
+                continue
+
+            if raw_block and l == "Parsed result:":
+                raw_block = False
+                parsed_block = True
+                continue
+
+            if parsed_block and not re.match("Found\s\d\sresult\spoints", l):
+                self.data += l + "\n"
+                continue
+
+            if parsed_block and re.match("Found\s\d\sresult\spoints", l):
+                parsed_block = False
+                point_block = True
+                continue
+
+            if point_block:
+                m = re.search("Point\s(\d+):\s\(([\d\.]+),([\d\.]+)\)", l)
+                if (m):
+                    self.points.append((float(m.group(2)), float(m.group(3))))
+
+        return
 
 
 if __name__ == "__main__":
-  print("ZXing module")
+    print("ZXing module")
