d1bc3cf2f2f8104d5b4b28ff19fee593fd6b17cc
lcd1232 <8745863+lcd1232@users.noreply.github.com>
2018-01-23 02:41:01 +0300

    Move all logic to barcode.py, improve version logic

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
  
 from distutils.core import setup
+import zxing
 
 setup(
     name='zxing',
-    version='0.1',
+    version=zxing.__version__,
     description="wrapper for zebra crossing (zxing) barcode library",
     url='http://simplecv.org',
     author='Ingenuitas',
     author_email='public.relations@ingenuitas.com',
     packages=['zxing'],
+    requires=['future']
 )
---
+++ zxing/barcode.py
@@ -0,0 +1,111 @@
+from future.utils import python_2_unicode_compatible
+import os
+import re
+import subprocess
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class BarCodeReader(object):
+    location = ""
+    command = "java"
+    libs = ["javase.jar", "core.jar", "jcommander.jar"]
+    args = ["-cp", "LIBS", "com.google.zxing.client.j2se.CommandLineRunner"]
+
+    def __init__(self, path=None):
+        if path is None:
+            if os.environ.get("ZXING_LIBRARY"):
+                path = os.environ["ZXING_LIBRARY"]
+            else:
+                path = ".."
+
+        self.location = path
+
+    def decode(self, files, try_harder=False, qr_only=False):
+        cmd = [self.command]
+        cmd += self.args[:]  # copy arg values
+        if try_harder:
+            cmd.append("--try_harder")
+        if qr_only:
+            cmd.append("--possibleFormats=QR_CODE")
+
+        libraries = [self.location + "/" + l for l in self.libs]
+
+        cmd = [c if c != "LIBS" else os.pathsep.join(libraries) for c in cmd]
+
+        # send one file, or multiple files in a list
+        single_file = False
+        if not isinstance(files, (list, tuple)):
+            cmd.append(files)
+            single_file = True
+        else:
+            cmd.extend(files)
+
+        (stdout, stderr) = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True).communicate()
+        codes = []
+        file_results = stdout.split("\nfile:")
+        for result in file_results:
+            lines = stdout.split("\n")
+            if re.search("No barcode found", lines[0]):
+                codes.append(None)
+                continue
+
+            codes.append(BarCode(result))
+
+        if single_file:
+            return codes[0]
+        else:
+            return codes
+
+
+# this is the barcode class which has
+class BarCode(object):
+    format = ""
+    points = []
+    data = ""
+    raw = ""
+
+    def __init__(self, zxing_output):
+        lines = zxing_output.split("\n")
+        raw_block = False
+        parsed_block = False
+        point_block = False
+
+        self.points = []
+        for l in lines:
+            m = re.search("format:\s([^,]+)", l)
+            if not raw_block and not parsed_block and not point_block and m:
+                self.format = m.group(1)
+                continue
+
+            if not raw_block and not parsed_block and not point_block and l == "Raw result:":
+                raw_block = True
+                continue
+
+            if raw_block and l != "Parsed result:":
+                self.raw += l + "\n"
+                continue
+
+            if raw_block and l == "Parsed result:":
+                raw_block = False
+                parsed_block = True
+                continue
+
+            if parsed_block and not re.match("Found\s\d\sresult\spoints", l):
+                self.data += l + "\n"
+                continue
+
+            if parsed_block and re.match("Found\s\d\sresult\spoints", l):
+                parsed_block = False
+                point_block = True
+                continue
+
+            if point_block:
+                m = re.search("Point\s(\d+):\s\(([\d\.]+),([\d\.]+)\)", l)
+                if m:
+                    self.points.append((float(m.group(2)), float(m.group(3))))
+
+    @python_2_unicode_compatible
+    def __str__(self):
+        return ''
